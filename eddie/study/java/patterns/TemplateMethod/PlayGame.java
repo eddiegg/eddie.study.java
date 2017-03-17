085da0cb6b445cb3973f1d7782ff32fe79f49838
eddy_gg@163.com <eddy_gg@163.com>
2017-03-17 17:11:10 +0800

    pattern

--- eddie/study/java/patterns/TemplateMethod/PlayGame.java
+++ eddie/study/java/patterns/TemplateMethod/PlayGame.java
@@ -2,6 +2,7 @@ package eddie.study.java.patterns.TemplateMethod;
 
 /**
  * Created by eddie on 2017/3/17.
+ * Java 8的接口可以有default和static实现（和抽象相反）方法，大部分情况下可以替代抽象类
  */
 public abstract class PlayGame {
     public abstract void openGameMachine();
