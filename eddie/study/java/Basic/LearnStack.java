0e37ca873598885595e7fe4eaf99366ab28f1791
eddy_gg@163.com <eddy_gg@163.com>
2017-03-30 13:39:54 +0800

    pattern

---
+++ eddie/study/java/Basic/LearnStack.java
@@ -0,0 +1,14 @@
+package eddie.study.java.Basic;
+
+import java.util.Stack;
+
+/**
+ * Created by eddie on 2017/3/30.
+ */
+public class LearnStack {
+    public static void main(String[] args) {
+        Stack<String> st = new Stack<>();
+        st.push("5");
+        System.out.println(st.pop());
+    }
+}
